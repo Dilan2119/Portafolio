9a1e45d787162b53ce396335691054150125ce2e
Dilan2119 <dilanbulding19@gmail.com>
2025-02-11 12:44:06 -0500

    Se agrego icono

--- data/skill.js
+++ data/skill.js
@@ -1,47 +1,51 @@
 const skills = [
-    {
-      name: 'HTML',
-      icon: 'iconos/iconohtml.svg'
-    },
-    {
-      name: 'CSS',
-      icon: 'iconos/iconocss.svg'
-    },
-    {
-      name: 'JavaScript',
-      icon: 'iconos/iconosjs.svg'
-    },
-    {
-        name: 'Bootstrap',
-        icon: 'iconos/iconoboot.svg'
-      },
-      {
-        name: 'php',
-        icon: 'iconos/iconophp.svg'
-      },
-      {
-        name: 'Sass',
-        icon: 'iconos/iconosass.svg'
-    },
-    {
-        name: 'MySQL',
-        icon: 'iconos/iconossql.svg'
-      },
-      {
-        name: 'Git',
-        icon: 'iconos/iconogit.svg'
-      },
-      {
-        name: 'React',
-        icon: 'iconos/iconoreact.svg'
-    },
-    {
-        name: 'Flutter',
-        icon: 'iconos/iconoflu.svg'
-    },
-    {
-        name: 'Python',
-        icon: 'iconos/iconopy.svg'
-    },
-    // Añade más habilidades si lo deseas
-  ];
+  {
+    name: "HTML",
+    icon: "iconos/iconohtml.svg",
+  },
+  {
+    name: "CSS",
+    icon: "iconos/iconocss.svg",
+  },
+  {
+    name: "JavaScript",
+    icon: "iconos/iconosjs.svg",
+  },
+  {
+    name: "Bootstrap",
+    icon: "iconos/iconoboot.svg",
+  },
+  {
+    name: "php",
+    icon: "iconos/iconophp.svg",
+  },
+  {
+    name: "Sass",
+    icon: "iconos/iconosass.svg",
+  },
+  {
+    name: "MySQL",
+    icon: "iconos/iconossql.svg",
+  },
+  {
+    name: "Git",
+    icon: "iconos/iconogit.svg",
+  },
+  {
+    name: "React",
+    icon: "iconos/iconoreact.svg",
+  },
+  {
+    name: "Flutter",
+    icon: "iconos/iconoflu.svg",
+  },
+  {
+    name: "Python",
+    icon: "iconos/iconopy.svg",
+  },
+  {
+    name: "Tailwind",
+    icon: "iconos/iconotail.svg",
+  },
+  // Añade más habilidades si lo deseas
+];
